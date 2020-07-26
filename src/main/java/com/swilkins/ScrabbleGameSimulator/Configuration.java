6e276e869f0d5b6110b81f0abfebb57dc328093f
Sam Wilkins <samwilkins333@gmail.com>
2020-07-25 21:25:50 -0700

    Changed default configuration

--- src/main/java/com/swilkins/ScrabbleGameSimulator/Configuration.java
+++ src/main/java/com/swilkins/ScrabbleGameSimulator/Configuration.java
@@ -3,8 +3,8 @@ package com.swilkins.ScrabbleGameSimulator;
 public final class Configuration {
 
   public static final int RACK_CAPACITY = 7;
-  public static final boolean ENABLE_LOGGING = false;
-  public static final int GAME_ITERATIONS = 10;
+  public static final boolean ENABLE_LOGGING = true;
+  public static final int GAME_ITERATIONS = 1;
   public static final String DICTIONARY = "ospd4.txt";
 
 }
